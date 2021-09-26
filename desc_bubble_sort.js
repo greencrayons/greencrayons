231b8da2031531da97c323b5df3475cb0b235a5b
greencrayons <91235233+greencrayons@users.noreply.github.com>
2021-09-26 00:51:32 -0400

    update

--- desc_bubble_sort.js
+++ desc_bubble_sort.js
@@ -1,46 +1,45 @@
-
-
-
-const bubble = (array) => {
-
- // array [input unsorted array of integers, index]
- array = [array, 0]
-
- let sort = () => {
-
-	// switchit transposes the current index with the next index
-	let switchit = () => {
-	array[0].splice(array[1] + 2, 0, array[0][array[1]])
-	array[0].splice(array[1], 1)
-	array[1] = 0
-	sort()
+	
+	
+	
+	const bubble = (array,index) => {
+	
+	 let sort = () => {
+	
+		// switchit transposes the current index with the next index
+		let switchit = () => {
+		array.splice(index + 2, 0, array[index])
+		array.splice(index, 1)
+		index = 0
+		};
+		
+	    // update() increments the current iteration index then calls sort() recursively
+		let update = () => {
+		index++
+		};
+	
+		
+		console.log(array)
+	    // if the index reaches the end of the array escape sort()
+		if(array.length == index + 1){
+		return
+		};
+		
+	    // compare the current index > next index XOR current index == next index will call update() if true, else call switchit()
+		((array[index] > array[index + 1]) ^ (array[index] == array[index + 1])) ? update():switchit(); 
+		
+		// call sort()
+		sort()
+	
+	 };
+	
+	 // inital sort() call
+	 sort()
+	 // return sorted descending array
+	 return array
+	
 	};
-
-        // update() increments the current iteration index then calls sort() recursively
-	let update = () => {
-	array[1]++
-	sort()
-	};
-
-
-	console.log(array)
-        // if the index reaches the end of the array escape sort()
-	if(array[0].length == array[1] + 1){
-	return
-	}
-
-        // compare the current index with the next index and call update() if true, else call switchit()
-	array[0][array[1]] > array[0][array[1] + 1] ? update():switchit();
-
-
- };
-
- // inital sort() call
- sort()
- // return sorted descending array
- return array[0]
-
-};
-
-
-bubble([1,2,3,4,8,12]);
+	
+	
+	bubble([1,2,3,4,8,8,12],0);
+	
+	
